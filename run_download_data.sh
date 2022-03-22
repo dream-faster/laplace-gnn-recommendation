94cdb5ba2b8b89575995d66a6df15fc012111b67
Mark Aron Szulyovszky <mark.szulyovszky@gmail.com>
2022-03-22 16:25:35 +0100

    feat(Data): added run_download_data.sh

---
+++ run_download_data.sh
@@ -0,0 +1,4 @@
+
+wget -P data/ https://storage.googleapis.com/heii-public/customers.parquet
+wget -P data/ https://storage.googleapis.com/heii-public/articles.parquet
+wget -P data/ https://storage.googleapis.com/heii-public/transactions_train.parquet
