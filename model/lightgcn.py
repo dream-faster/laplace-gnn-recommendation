51a6584660d595ea018a73058bb4b36e057076bb
Mark Aron Szulyovszky <mark.szulyovszky@gmail.com>
2022-03-25 14:47:11 +0100

    feat(Pipeline): added train/validation from lightgcn tutorial

---
+++ model/lightgcn.py
@@ -0,0 +1,194 @@
+"""
+Defines the architecture of the full LightGCN model.
+"""
+
+import torch
+from torch_geometric.nn import MessagePassing
+from torch_geometric.utils import degree
+from utils.metrics import recall_at_k
+
+
+class GNN(torch.nn.Module):
+    """
+    Overall graph neural network. Consists of learnable user/item (i.e., playlist/song) embeddings
+    and LightGCN layers.
+    """
+
+    def __init__(self, embedding_dim, num_nodes, num_playlists, num_layers):
+        super(GNN, self).__init__()
+
+        self.embedding_dim = embedding_dim
+        self.num_nodes = (
+            num_nodes  # total number of nodes (songs + playlists) in dataset
+        )
+        self.num_playlists = num_playlists  # total number of playlists in dataset
+        self.num_layers = num_layers
+
+        # Initialize embeddings for all playlists and songs. Playlists will have indices from 0...num_playlists-1,
+        # songs will have indices from num_playlists...num_nodes-1
+        self.embeddings = torch.nn.Embedding(
+            num_embeddings=self.num_nodes, embedding_dim=self.embedding_dim
+        )
+        torch.nn.init.normal_(self.embeddings.weight, std=0.1)
+
+        self.layers = torch.nn.ModuleList()  # LightGCN layers
+        for _ in range(self.num_layers):
+            self.layers.append(LightGCN())
+
+        self.sigmoid = torch.sigmoid
+
+    def forward(self):
+        raise NotImplementedError(
+            "forward() has not been implemented for the GNN class. Do not use"
+        )
+
+    def gnn_propagation(self, edge_index_mp):
+        """
+        Performs the linear embedding propagation (using the LightGCN layers) and calculates final (multi-scale) embeddings
+        for each user/item, which are calculated as a weighted sum of that user/item's embeddings at each layer (from
+        0 to self.num_layers). Technically, the weighted sum here is the average, which is what the LightGCN authors recommend.
+        args:
+          edge_index_mp: a tensor of all (undirected) edges in the graph, which is used for message passing/propagation and
+              calculating the multi-scale embeddings. (In contrast to the evaluation/supervision edges, which are distinct
+              from the message passing edges and will be used for calculating loss/performance metrics).
+        returns:
+          final multi-scale embeddings for all users/items
+        """
+        x = self.embeddings.weight  # layer-0 embeddings
+
+        x_at_each_layer = [
+            x
+        ]  # stores embeddings from each layer. Start with layer-0 embeddings
+        for i in range(self.num_layers):  # now performing the GNN propagation
+            x = self.layers[i](x, edge_index_mp)
+            x_at_each_layer.append(x)
+        final_embs = torch.stack(x_at_each_layer, dim=0).mean(
+            dim=0
+        )  # take average to calculate multi-scale embeddings
+        return final_embs
+
+    def predict_scores(self, edge_index, embs):
+        """
+        Calculates predicted scores for each playlist/song pair in the list of edges. Uses dot product of their embeddings.
+        args:
+          edge_index: tensor of edges (between playlists and songs) whose scores we will calculate.
+          embs: node embeddings for calculating predicted scores (typically the multi-scale embeddings from gnn_propagation())
+        returns:
+          predicted scores for each playlist/song pair in edge_index
+        """
+        scores = (
+            embs[edge_index[0, :], :] * embs[edge_index[1, :], :]
+        )  # taking dot product for each playlist/song pair
+        scores = scores.sum(dim=1)
+        scores = self.sigmoid(scores)
+        return scores
+
+    def calc_loss(self, data_mp, data_pos, data_neg):
+        """
+        The main training step. Performs GNN propagation on message passing edges, to get multi-scale embeddings.
+        Then predicts scores for each training example, and calculates Bayesian Personalized Ranking (BPR) loss.
+        args:
+          data_mp: tensor of edges used for message passing / calculating multi-scale embeddings
+          data_pos: set of positive edges that will be used during loss calculation
+          data_neg: set of negative edges that will be used during loss calculation
+        returns:
+          loss calculated on the positive/negative training edges
+        """
+        # Perform GNN propagation on message passing edges to get final embeddings
+        final_embs = self.gnn_propagation(data_mp.edge_index)
+
+        # Get edge prediction scores for all positive and negative evaluation edges
+        pos_scores = self.predict_scores(data_pos.edge_index, final_embs)
+        neg_scores = self.predict_scores(data_neg.edge_index, final_embs)
+
+        # # Calculate loss (binary cross-entropy). Commenting out, but can use instead of BPR if desired.
+        # all_scores = torch.cat([pos_scores, neg_scores], dim=0)
+        # all_labels = torch.cat([torch.ones(pos_scores.shape[0]), torch.zeros(neg_scores.shape[0])], dim=0)
+        # loss_fn = torch.nn.BCELoss()
+        # loss = loss_fn(all_scores, all_labels)
+
+        # Calculate loss (using variation of Bayesian Personalized Ranking loss, similar to the one used in official
+        # LightGCN implementation at https://github.com/gusye1234/LightGCN-PyTorch/blob/master/code/model.py#L202)
+        loss = -torch.log(self.sigmoid(pos_scores - neg_scores)).mean()
+        return loss
+
+    def evaluation(self, data_mp, data_pos, k):
+        """
+        Performs evaluation on validation or test set. Calculates recall@k.
+        args:
+          data_mp: message passing edges to use for propagation/calculating multi-scale embeddings
+          data_pos: positive edges to use for scoring metrics. Should be no overlap between these edges and data_mp's edges
+          k: value of k to use for recall@k
+        returns:
+          dictionary mapping playlist ID -> recall@k on that playlist
+        """
+        # Run propagation on the message-passing edges to get multi-scale embeddings
+        final_embs = self.gnn_propagation(data_mp.edge_index)
+
+        # Get embeddings of all unique playlists in the batch of evaluation edges
+        unique_playlists = torch.unique_consecutive(data_pos.edge_index[0, :])
+        playlist_emb = final_embs[
+            unique_playlists, :
+        ]  # has shape [number of playlists in batch, 64]
+
+        # Get embeddings of ALL songs in dataset
+        song_emb = final_embs[
+            self.num_playlists :, :
+        ]  # has shape [total number of songs in dataset, 64]
+
+        # All ratings for each playlist in batch to each song in entire dataset (using dot product as the scoring function)
+        ratings = self.sigmoid(
+            torch.matmul(playlist_emb, song_emb.t())
+        )  # shape: [# playlists in batch, # songs in dataset]
+        # where entry i,j is rating of song j for playlist i
+        # Calculate recall@k
+        result = recall_at_k(
+            ratings.cpu(),
+            k,
+            self.num_playlists,
+            data_pos.edge_index.cpu(),
+            unique_playlists.cpu(),
+            data_mp.edge_index.cpu(),
+        )
+        return result
+
+
+class LightGCN(MessagePassing):
+    """
+    A single LightGCN layer. Extends the MessagePassing class from PyTorch Geometric
+    """
+
+    def __init__(self):
+        super(LightGCN, self).__init__(aggr="add")  # aggregation function is 'add'
+
+    def message(self, x_j, norm):
+        """
+        Specifies how to perform message passing during GNN propagation. For LightGCN, we simply pass along each
+        source node's embedding to the target node, normalized by the normalization term for that node.
+        args:
+          x_j: node embeddings of the neighbor nodes, which will be passed to the central node (shape: [E, emb_dim])
+          norm: the normalization terms we calculated in forward() and passed into propagate()
+        returns:
+          messages from neighboring nodes j to central node i
+        """
+        # Here we are just multiplying the x_j's by the normalization terms (using some broadcasting)
+        return norm.view(-1, 1) * x_j
+
+    def forward(self, x, edge_index):
+        """
+        Performs the LightGCN message passing/aggregation/update to get updated node embeddings
+        args:
+          x: current node embeddings (shape: [N, emb_dim])
+          edge_index: message passing edges (shape: [2, E])
+        returns:
+          updated embeddings after this layer
+        """
+        # Computing node degrees for normalization term in LightGCN (see LightGCN paper for details on this normalization term)
+        # These will be used during message passing, to normalize each neighbor's embedding before passing it as a message
+        row, col = edge_index
+        deg = degree(col)
+        deg_inv_sqrt = deg.pow(-0.5)
+        norm = deg_inv_sqrt[row] * deg_inv_sqrt[col]
+
+        # Begin propagation. Will perform message passing and aggregation and return updated node embeddings.
+        return self.propagate(edge_index, x=x, norm=norm)
--- run_pipeline.py
+++ run_pipeline.py
@@ -1,5 +1,150 @@
 from data.types import DataLoaderConfig
 from data.data_loader import run_dataloader
+from torch_geometric import seed_everything
+import torch
+from torch_geometric.loader import DataLoader
+from model.lightgcn import GNN
+import os
+import numpy as np
 
-config = DataLoaderConfig(test_split=0.15, val_split=0.15)
-train, val, test = run_dataloader(config)
+
+def train(model, data_mp, loader, opt, num_playlists, num_nodes, device):
+    """
+    Main training loop
+    args:
+       model: the GNN model
+       data_mp: message passing edges to use for performing propagation/calculating multi-scale embeddings
+       loader: DataLoader that loads in batches of supervision/evaluation edges
+       opt: the optimizer
+       num_playlists: the number of playlists in the entire dataset
+       num_nodes: the number of nodes (playlists + songs) in the entire dataset
+       device: whether to run on CPU or GPU
+    returns:
+       the training loss for this epoch
+    """
+    total_loss = 0
+    total_examples = 0
+    model.train()
+    for batch in loader:
+        del batch.batch
+        del batch.ptr  # delete unwanted attributes
+
+        opt.zero_grad()
+        negs = sample_negative_edges(
+            batch, num_playlists, num_nodes
+        )  # sample negative edges
+        data_mp, batch, negs = data_mp.to(device), batch.to(device), negs.to(device)
+        loss = model.calc_loss(data_mp, batch, negs)
+        loss.backward()
+        opt.step()
+
+        num_examples = batch.edge_index.shape[1]
+        total_loss += loss.item() * num_examples
+        total_examples += num_examples
+    avg_loss = total_loss / total_examples
+    return avg_loss
+
+
+def test(model, data_mp, loader, k, device, save_dir, epoch):
+    """
+    Evaluation loop for validation/testing.
+    args:
+       model: the GNN model
+       data_mp: message passing edges to use for propagation/calculating multi-scale embeddings
+       loader: DataLoader that loads in batches of evaluation (i.e., validation or test) edges
+       k: value of k to use for recall@k
+       device: whether to use CPU or GPU
+       save_dir: directory to save multi-scale embeddings for later analysis. If None, doesn't save any embeddings.
+       epoch: the number of the current epoch
+    returns:
+       recall@k for this epoch
+    """
+    model.eval()
+    all_recalls = {}
+    with torch.no_grad():
+        # Save multi-scale embeddings if save_dir is not None
+        data_mp = data_mp.to(device)
+        if save_dir is not None:
+            embs_to_save = gnn.gnn_propagation(data_mp.edge_index)
+            torch.save(
+                embs_to_save, os.path.join(save_dir, f"embeddings_epoch_{epoch}.pt")
+            )
+
+        # Run evaluation
+        for batch in loader:
+            del batch.batch
+            del batch.ptr  # delete unwanted attributes
+
+            batch = batch.to(device)
+            recalls = model.evaluation(data_mp, batch, k)
+            for playlist_idx in recalls:
+                assert playlist_idx not in all_recalls
+            all_recalls.update(recalls)
+    recall_at_k = np.mean(list(all_recalls.values()))
+    return recall_at_k
+
+
+def run_pipeline():
+    seed_everything(5)
+
+    config = DataLoaderConfig(test_split=0.15, val_split=0.15)
+    train_data, val_data, test_data = run_dataloader(config)
+
+    # Training hyperparameters
+    epochs = 300  # number of training epochs
+    k = 250  # value of k for recall@k. It is important to set this to a reasonable value!
+    num_layers = 3  # number of LightGCN layers (i.e., number of hops to consider during propagation)
+    batch_size = 2048  # batch size. refers to the # of playlists in the batch (each will come with all of its edges)
+    embedding_dim = 64  # dimension to use for the playlist/song embeddings
+    save_emb_dir = None  # path to save multi-scale embeddings during test(). If None, will not save any embeddings
+
+    # Use GPU if available
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    # Create DataLoaders for the supervision/evaluation edges (one each for train/val/test sets)
+    train_loader = DataLoader(train_data[0], batch_size=batch_size, shuffle=True)
+    val_loader = DataLoader(val_data[0], batch_size=batch_size, shuffle=False)
+    test_loader = DataLoader(test_data[0], batch_size=batch_size, shuffle=False)
+
+    # Initialize GNN model
+    gnn = GNN(
+        embedding_dim=embedding_dim,
+        num_nodes=data.num_nodes,
+        num_playlists=num_playlists,
+        num_layers=num_layers,
+    ).to(device)
+
+    opt = torch.optim.Adam(gnn.parameters(), lr=1e-3)  # using Adam optimizer
+
+    all_train_losses = []  # list of (epoch, training loss)
+    all_val_recalls = []  # list of (epoch, validation recall@k)
+
+    # Main training loop
+    for epoch in range(epochs):
+        train_loss = train(
+            gnn, train_mp, train_loader, opt, num_playlists, num_nodes, device
+        )
+        all_train_losses.append((epoch, train_loss))
+
+        if epoch % 5 == 0:
+            val_recall = test(gnn, val_mp, val_loader, k, device, save_emb_dir, epoch)
+            all_val_recalls.append((epoch, val_recall))
+            print(f"Epoch {epoch}: train loss={train_loss}, val_recall={val_recall}")
+        else:
+            print(f"Epoch {epoch}: train loss={train_loss}")
+
+    print()
+
+    # Print best validation recall@k value
+    best_val_recall = max(all_val_recalls, key=lambda x: x[1])
+    print(
+        f"Best validation recall@k: {best_val_recall[1]} at epoch {best_val_recall[0]}"
+    )
+
+    # Print final recall@k on test set
+    test_recall = test(gnn, test_mp, test_loader, k, device, None, None)
+    print(f"Test set recall@k: {test_recall}")
+
+
+if __name__ == "__main__":
+    run_pipeline()
