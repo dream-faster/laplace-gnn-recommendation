12f376062b79ee15640b7901710c046078358352
Mark Aron Szulyovszky <mark.szulyovszky@gmail.com>
2022-04-07 11:22:14 +0200

    feat(Preprocessing): added `create_datasets()` function

--- data/data_loader.py
+++ data/data_loader.py
@@ -73,6 +73,49 @@ def create_dataloaders(
     )
 
 
+def create_datasets(
+    config: DataLoaderConfig,
+) -> Tuple[HeteroData, HeteroData, HeteroData, CustomerIdMap, ArticleIdMap]:
+    data = torch.load("data/derived/graph.pt")
+    # Add a reverse ('article', 'rev_buys', 'customer') relation for message passing:
+    data = T.ToUndirected()(data)
+    del data["article", "rev_buys", "customer"].edge_label  # Remove "reverse" label.
+
+    # from torch_geometric.datasets import MovieLens
+    # import os.path as osp
+
+    # path = osp.join(osp.dirname(osp.realpath(__file__)), "data/MovieLens")
+    # data_mov = MovieLens(path, model_name="all-MiniLM-L6-v2")[0]
+
+    transform = RandomLinkSplit(
+        is_undirected=True,
+        add_negative_train_samples=False,
+        num_val=config.val_split,
+        num_test=config.test_split,
+        neg_sampling_ratio=0,
+        edge_types=[("customer", "buys", "article")],
+        rev_edge_types=[("article", "rev_buys", "customer")],
+    )
+    train_split, val_split, test_split = transform(data)
+
+    # Confirm that every node appears in every set above
+    assert (
+        train_split.num_nodes == val_split.num_nodes
+        and train_split.num_nodes == test_split.num_nodes
+    )
+
+    customer_id_map = read_json("data/derived/customer_id_map_forward.json")
+    article_id_map = read_json("data/derived/article_id_map_forward.json")
+
+    return (
+        train_split,
+        val_split,
+        test_split,
+        customer_id_map,
+        article_id_map,
+    )
+
+
 def read_json(filename: str):
     with open(filename) as f_in:
         return json.load(f_in)
