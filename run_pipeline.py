8022d49f715fdba28235e5a88ed58e6b937daa2e
Mark Aron Szulyovszky <mark.szulyovszky@gmail.com>
2022-04-13 16:27:33 +0200

    fix(Config): batch_size was set as a constant, adjusted test/val split ratio for smaller graph

--- run_pipeline.py
+++ run_pipeline.py
@@ -48,7 +48,9 @@ def run_pipeline(config: Config):
         customer_id_map,
         article_id_map,
         full_data,
-    ) = loader(DataLoaderConfig(test_split=0.001, val_split=0.001, batch_size=32))
+    ) = loader(
+        DataLoaderConfig(test_split=0.01, val_split=0.01, batch_size=config.batch_size)
+    )
 
     print(
         "--- Data Type: {} ---".format(
