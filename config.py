0a7e054c15fb237d2fc1b08178a6f72a7ac5080a
Mark Aron Szulyovszky <mark.szulyovszky@gmail.com>
2022-04-13 16:18:33 +0200

    feat(Pipeline): train on large graph

--- config.py
+++ config.py
@@ -13,25 +13,23 @@ from data.types import (
 class Config:
     epochs: int  # number of training epochs
     k: int  # value of k for recall@k. It is important to set this to a reasonable value!
-    num_layers: int  # number of LightGCN layers (i.e., number of hops to consider during propagation)
+    # num_layers: int  # number of  layers (i.e., number of hops to consider during propagation)
     batch_size: int  # batch size. refers to the # of customers in the batch (each will come with all of its edges)
     embedding_dim: int  # dimension to use for the customer/article embeddings
-    save_emb_dir: Optional[
-        str
-    ]  # path to save multi-scale embeddings during test(). If None, will not save any embeddings
     type: GraphType  # type of graph we use
     dataloader: bool
+    save_model: bool
 
 
 config = Config(
-    epochs=1500,
+    epochs=1,
     k=12,
-    num_layers=3,
-    batch_size=1,
+    # num_layers=3,
+    batch_size=512,
     embedding_dim=64,
-    save_emb_dir=None,
     type=GraphType.heterogenous,
     dataloader=True,
+    save_model=False,
 )
 
 only_users_and_articles_nodes = PreprocessingConfig(
@@ -57,7 +55,7 @@ only_users_and_articles_nodes = PreprocessingConfig(
     load_text_embedding=False,
     text_embedding_colname="derived_look",
     K=0,
-    data_size=1000,
+    data_size=10000000,
     save_to_csv=False,
     data_type=DataType.pyg,
 )
