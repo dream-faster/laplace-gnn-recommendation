fa9608235abf77492e2a4fcccb661755dba475f4
Daniel Szemerey <daniel.szemerey.15@ucl.ac.uk>
2022-04-20 10:13:52 +0200

    chore: Refractored jupyter notebook to be more digestable.

---
+++ run_pipeline_lightgcn.py
@@ -0,0 +1,299 @@
+# Commented out IPython magic to ensure Python compatibility.
+# # Install required packages.
+# %%capture
+# !pip install torch-scatter -f https://data.pyg.org/whl/torch-1.10.0+cu111.html
+# !pip install torch-sparse -f https://data.pyg.org/whl/torch-1.10.0+cu111.html
+# !pip install torch-geometric
+# !pip install -q git+https://github.com/snap-stanford/deepsnap.git
+# !pip install -U -q PyDrive
+
+"""# Implementing a Recommender System using LightGCN
+
+In this colab, we explain how to set up a graph recommender system using the [LighGCN](https://arxiv.org/abs/2002.02126) model. Specifically, we apply LightGCN to a movie recommendation task using [PyTorch](https://pytorch.org/) and [PyTorch Geometric](https://pytorch-geometric.readthedocs.io/en/latest/).
+
+We use the [MovieLens](https://grouplens.org/datasets/movielens/) (*small*) dataset which has 100,000 ratings applied to 9,000 movies by 600 users. 
+
+Our implementation was inspired by the following documentation and repositories:
+- https://github.com/gusye1234/LightGCN-PyTorch
+- https://www.kaggle.com/dipanjandas96/lightgcn-pytorch-from-scratch
+- https://pytorch-geometric.readthedocs.io/en/latest/notes/load_csv.html
+"""
+
+# import required modules
+import random
+from tqdm import tqdm
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+
+import torch
+from torch import nn, optim, Tensor
+
+from torch_sparse import SparseTensor, matmul
+
+from torch_geometric.utils import structured_negative_sampling
+from torch_geometric.data import download_url, extract_zip
+
+
+from torch_geometric.typing import Adj
+from model.lightgcn import LightGCN
+from data.movie_loader import create_dataloaders, sample_mini_batch
+from utils.lightgcn_metrics import (
+    get_metrics,
+    bpr_loss,
+    RecallPrecision_ATk,
+    NDCGatK_r,
+    get_user_positive_items,
+)
+
+movie_path = "./data/movie/ml-latest-small/movies.csv"
+rating_path = "./data/movie/ml-latest-small/ratings.csv"
+
+
+# wrapper function to evaluate model
+def evaluation(
+    model, edge_index, sparse_edge_index, exclude_edge_indices, k, lambda_val
+):
+    """Evaluates model loss and metrics including recall, precision, ndcg @ k
+
+    Args:
+        model (LighGCN): lightgcn model
+        edge_index (torch.Tensor): 2 by N list of edges for split to evaluate
+        sparse_edge_index (sparseTensor): sparse adjacency matrix for split to evaluate
+        exclude_edge_indices ([type]): 2 by N list of edges for split to discount from evaluation
+        k (int): determines the top k items to compute metrics on
+        lambda_val (float): determines lambda for bpr loss
+
+    Returns:
+        tuple: bpr loss, recall @ k, precision @ k, ndcg @ k
+    """
+    # get embeddings
+    users_emb_final, users_emb_0, items_emb_final, items_emb_0 = model.forward(
+        sparse_edge_index
+    )
+    edges = structured_negative_sampling(edge_index, contains_neg_self_loops=False)
+    user_indices, pos_item_indices, neg_item_indices = edges[0], edges[1], edges[2]
+    users_emb_final, users_emb_0 = (
+        users_emb_final[user_indices],
+        users_emb_0[user_indices],
+    )
+    pos_items_emb_final, pos_items_emb_0 = (
+        items_emb_final[pos_item_indices],
+        items_emb_0[pos_item_indices],
+    )
+    neg_items_emb_final, neg_items_emb_0 = (
+        items_emb_final[neg_item_indices],
+        items_emb_0[neg_item_indices],
+    )
+
+    loss = bpr_loss(
+        users_emb_final,
+        users_emb_0,
+        pos_items_emb_final,
+        pos_items_emb_0,
+        neg_items_emb_final,
+        neg_items_emb_0,
+        lambda_val,
+    ).item()
+
+    recall, precision, ndcg = get_metrics(model, edge_index, exclude_edge_indices, k)
+
+    return loss, recall, precision, ndcg
+
+
+def train():
+    """# Training
+
+    Your test set performance should be in line with the following (*K=20*):
+
+    *Recall@K: 0.13, Precision@K: 0.045, NDCG@K: 0.10*
+    """
+    (
+        train_sparse_edge_index,
+        val_sparse_edge_index,
+        test_sparse_edge_index,
+        train_edge_index,
+        val_edge_index,
+        test_edge_index,
+        edge_index,
+        user_mapping,
+        movie_mapping,
+        num_users,
+        num_movies,
+    ) = create_dataloaders(movie_path, rating_path)
+    # define contants
+    ITERATIONS = 10000
+    BATCH_SIZE = 1024
+    LR = 1e-3
+    ITERS_PER_EVAL = 200
+    ITERS_PER_LR_DECAY = 200
+    K = 20
+    LAMBDA = 1e-6
+
+    # setup
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print(f"Using device {device}.")
+
+    model = LightGCN(num_users, num_movies)
+    model = model.to(device)
+    model.train()
+
+    optimizer = optim.Adam(model.parameters(), lr=LR)
+    scheduler = optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.95)
+
+    edge_index = edge_index.to(device)
+    train_edge_index = train_edge_index.to(device)
+    train_sparse_edge_index = train_sparse_edge_index.to(device)
+
+    val_edge_index = val_edge_index.to(device)
+    val_sparse_edge_index = val_sparse_edge_index.to(device)
+
+    # training loop
+    train_losses = []
+    val_losses = []
+
+    for iter in range(ITERATIONS):
+        # forward propagation
+        users_emb_final, users_emb_0, items_emb_final, items_emb_0 = model.forward(
+            train_sparse_edge_index
+        )
+
+        # mini batching
+        user_indices, pos_item_indices, neg_item_indices = sample_mini_batch(
+            BATCH_SIZE, train_edge_index
+        )
+        user_indices, pos_item_indices, neg_item_indices = (
+            user_indices.to(device),
+            pos_item_indices.to(device),
+            neg_item_indices.to(device),
+        )
+        users_emb_final, users_emb_0 = (
+            users_emb_final[user_indices],
+            users_emb_0[user_indices],
+        )
+        pos_items_emb_final, pos_items_emb_0 = (
+            items_emb_final[pos_item_indices],
+            items_emb_0[pos_item_indices],
+        )
+        neg_items_emb_final, neg_items_emb_0 = (
+            items_emb_final[neg_item_indices],
+            items_emb_0[neg_item_indices],
+        )
+
+        # loss computation
+        train_loss = bpr_loss(
+            users_emb_final,
+            users_emb_0,
+            pos_items_emb_final,
+            pos_items_emb_0,
+            neg_items_emb_final,
+            neg_items_emb_0,
+            LAMBDA,
+        )
+
+        optimizer.zero_grad()
+        train_loss.backward()
+        optimizer.step()
+
+        if iter % ITERS_PER_EVAL == 0:
+            model.eval()
+            val_loss, recall, precision, ndcg = evaluation(
+                model,
+                val_edge_index,
+                val_sparse_edge_index,
+                [train_edge_index],
+                K,
+                LAMBDA,
+            )
+            print(
+                f"[Iteration {iter}/{ITERATIONS}] train_loss: {round(train_loss.item(), 5)}, val_loss: {round(val_loss, 5)}, val_recall@{K}: {round(recall, 5)}, val_precision@{K}: {round(precision, 5)}, val_ndcg@{K}: {round(ndcg, 5)}"
+            )
+            train_losses.append(train_loss.item())
+            val_losses.append(val_loss)
+            model.train()
+
+        if iter % ITERS_PER_LR_DECAY == 0 and iter != 0:
+            scheduler.step()
+
+    iters = [iter * ITERS_PER_EVAL for iter in range(len(train_losses))]
+    plt.plot(iters, train_losses, label="train")
+    plt.plot(iters, val_losses, label="validation")
+    plt.xlabel("iteration")
+    plt.ylabel("loss")
+    plt.title("training and validation loss curves")
+    plt.legend()
+    plt.show()
+
+    # evaluate on test set
+    model.eval()
+    test_edge_index = test_edge_index.to(device)
+    test_sparse_edge_index = test_sparse_edge_index.to(device)
+
+    test_loss, test_recall, test_precision, test_ndcg = evaluation(
+        model,
+        test_edge_index,
+        test_sparse_edge_index,
+        [train_edge_index, val_edge_index],
+        K,
+        LAMBDA,
+    )
+
+    print(
+        f"[test_loss: {round(test_loss, 5)}, test_recall@{K}: {round(test_recall, 5)}, test_precision@{K}: {round(test_precision, 5)}, test_ndcg@{K}: {round(test_ndcg, 5)}"
+    )
+
+    """# Make New Recommendatios for a Given User"""
+
+    model.eval()
+    df = pd.read_csv(movie_path)
+    movieid_title = pd.Series(df.title.values, index=df.movieId).to_dict()
+    movieid_genres = pd.Series(df.genres.values, index=df.movieId).to_dict()
+
+    user_pos_items = get_user_positive_items(edge_index)
+
+    def make_predictions(user_id, num_recs):
+        user = user_mapping[user_id]
+        e_u = model.users_emb.weight[user]
+        scores = model.items_emb.weight @ e_u
+
+        values, indices = torch.topk(scores, k=len(user_pos_items[user]) + num_recs)
+
+        movies = [
+            index.cpu().item() for index in indices if index in user_pos_items[user]
+        ][:num_recs]
+        movie_ids = [
+            list(movie_mapping.keys())[list(movie_mapping.values()).index(movie)]
+            for movie in movies
+        ]
+        titles = [movieid_title[id] for id in movie_ids]
+        genres = [movieid_genres[id] for id in movie_ids]
+
+        print(f"Here are some movies that user {user_id} rated highly")
+        for i in range(num_recs):
+            print(f"title: {titles[i]}, genres: {genres[i]} ")
+
+        print()
+
+        movies = [
+            index.cpu().item() for index in indices if index not in user_pos_items[user]
+        ][:num_recs]
+        movie_ids = [
+            list(movie_mapping.keys())[list(movie_mapping.values()).index(movie)]
+            for movie in movies
+        ]
+        titles = [movieid_title[id] for id in movie_ids]
+        genres = [movieid_genres[id] for id in movie_ids]
+
+        print(f"Here are some suggested movies for user {user_id}")
+        for i in range(num_recs):
+            print(f"title: {titles[i]}, genres: {genres[i]} ")
+
+    USER_ID = 1
+    NUM_RECS = 10
+
+    make_predictions(USER_ID, NUM_RECS)
+
+
+if __name__ == "__main__":
+    train()
---
+++ utils/lightgcn_metrics.py
@@ -0,0 +1,245 @@
+import random
+from tqdm import tqdm
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+
+import torch
+from torch import nn, optim, Tensor
+
+from torch_sparse import SparseTensor, matmul
+
+from torch_geometric.utils import structured_negative_sampling
+from torch_geometric.data import download_url, extract_zip
+
+
+from torch_geometric.typing import Adj
+
+
+"""# Loss Function
+
+
+
+We utilize a Bayesian Personalized Ranking (BPR) loss, a pairwise objective which encourages the predictions of positive samples to be higher than negative samples for each user.
+
+\begin{equation}
+L_{BPR} = -\sum_{u = 1}^M \sum_{i \in N_u} \sum_{j \notin N_u} \ln{\sigma(\hat{y}_{ui} - \hat{y}_{uj})} + \lambda ||E^{(0)}||^2 
+\end{equation}
+
+$\hat{y}_{u}$: predicted score of a positive sample
+
+$\hat{y}_{uj}$: predicted score of a negative sample
+
+$\lambda$: hyperparameter which controls the L2 regularization strength
+"""
+
+
+def bpr_loss(
+    users_emb_final,
+    users_emb_0,
+    pos_items_emb_final,
+    pos_items_emb_0,
+    neg_items_emb_final,
+    neg_items_emb_0,
+    lambda_val,
+):
+    """Bayesian Personalized Ranking Loss as described in https://arxiv.org/abs/1205.2618
+
+    Args:
+        users_emb_final (torch.Tensor): e_u_k
+        users_emb_0 (torch.Tensor): e_u_0
+        pos_items_emb_final (torch.Tensor): positive e_i_k
+        pos_items_emb_0 (torch.Tensor): positive e_i_0
+        neg_items_emb_final (torch.Tensor): negative e_i_k
+        neg_items_emb_0 (torch.Tensor): negative e_i_0
+        lambda_val (float): lambda value for regularization loss term
+
+    Returns:
+        torch.Tensor: scalar bpr loss value
+    """
+    reg_loss = lambda_val * (
+        users_emb_0.norm(2).pow(2)
+        + pos_items_emb_0.norm(2).pow(2)
+        + neg_items_emb_0.norm(2).pow(2)
+    )  # L2 loss
+
+    pos_scores = torch.mul(users_emb_final, pos_items_emb_final)
+    pos_scores = torch.sum(pos_scores, dim=-1)  # predicted scores of positive samples
+    neg_scores = torch.mul(users_emb_final, neg_items_emb_final)
+    neg_scores = torch.sum(neg_scores, dim=-1)  # predicted scores of negative samples
+
+    loss = -torch.mean(torch.nn.functional.softplus(pos_scores - neg_scores)) + reg_loss
+
+    return loss
+
+
+"""# Evaluation Metrics
+
+We evalaluate our model using the following metrics
+
+\begin{equation}
+\text{Recall} = \frac{TP}{TP + FP}
+\end{equation}
+
+\begin{equation}
+\text{Precision} = \frac{TP}{TP + FN}
+\end{equation}
+
+**Dicounted Cumulative Gain (DCG)** at rank position p is defined as:
+
+\begin{equation}
+\text{DCG}_\text{p} = \sum_{i = 1}^p \frac{2^{rel_i} - 1}{\log_2{(i + 1)}}
+\end{equation}
+
+p: a particular rank position
+
+$rel_i \in \{0, 1\}$ : graded relevance of the result at position $i$
+
+**Idealised Dicounted Cumulative Gain (IDCG)**, namely the maximum possible DCG, at rank position $p$ is defined as:
+
+\begin{equation}
+\text{IDCG}_\text{p} = \sum_{i = 1}^{|REL_p|} \frac{2^{rel_i} - 1}{\log_2{(i + 1)}}
+\end{equation}
+
+$|REL_p|$ : list of items ordered by their relevance up to position p
+
+**Normalized Dicounted Cumulative Gain (NDCG)** at rank position $p$ is defined as:
+
+\begin{equation}
+\text{nDCG}_\text{p} = \frac{\text{DCG}_p}{\text{nDCG}_p}
+\end{equation}
+
+Specifically, we use the metrics recall@K, precision@K, and NDCG@K. @K indicates that these metrics are computed on the top K recommendations.
+"""
+
+# helper function to get N_u
+def get_user_positive_items(edge_index):
+    """Generates dictionary of positive items for each user
+
+    Args:
+        edge_index (torch.Tensor): 2 by N list of edges
+
+    Returns:
+        dict: dictionary of positive items for each user
+    """
+    user_pos_items = {}
+    for i in range(edge_index.shape[1]):
+        user = edge_index[0][i].item()
+        item = edge_index[1][i].item()
+        if user not in user_pos_items:
+            user_pos_items[user] = []
+        user_pos_items[user].append(item)
+    return user_pos_items
+
+
+# computes recall@K and precision@K
+def RecallPrecision_ATk(groundTruth, r, k):
+    """Computers recall @ k and precision @ k
+
+    Args:
+        groundTruth (list): list of lists containing highly rated items of each user
+        r (list): list of lists indicating whether each top k item recommended to each user
+            is a top k ground truth item or not
+        k (intg): determines the top k items to compute precision and recall on
+
+    Returns:
+        tuple: recall @ k, precision @ k
+    """
+    num_correct_pred = torch.sum(
+        r, dim=-1
+    )  # number of correctly predicted items per user
+    # number of items liked by each user in the test set
+    user_num_liked = torch.Tensor(
+        [len(groundTruth[i]) for i in range(len(groundTruth))]
+    )
+    recall = torch.mean(num_correct_pred / user_num_liked)
+    precision = torch.mean(num_correct_pred) / k
+    return recall.item(), precision.item()
+
+
+# computes NDCG@K
+def NDCGatK_r(groundTruth, r, k):
+    """Computes Normalized Discounted Cumulative Gain (NDCG) @ k
+
+    Args:
+        groundTruth (list): list of lists containing highly rated items of each user
+        r (list): list of lists indicating whether each top k item recommended to each user
+            is a top k ground truth item or not
+        k (int): determines the top k items to compute ndcg on
+
+    Returns:
+        float: ndcg @ k
+    """
+    assert len(r) == len(groundTruth)
+
+    test_matrix = torch.zeros((len(r), k))
+
+    for i, items in enumerate(groundTruth):
+        length = min(len(items), k)
+        test_matrix[i, :length] = 1
+    max_r = test_matrix
+    idcg = torch.sum(max_r * 1.0 / torch.log2(torch.arange(2, k + 2)), axis=1)
+    dcg = r * (1.0 / torch.log2(torch.arange(2, k + 2)))
+    dcg = torch.sum(dcg, axis=1)
+    idcg[idcg == 0.0] = 1.0
+    ndcg = dcg / idcg
+    ndcg[torch.isnan(ndcg)] = 0.0
+    return torch.mean(ndcg).item()
+
+
+# wrapper function to get evaluation metrics
+def get_metrics(model, edge_index, exclude_edge_indices, k):
+    """Computes the evaluation metrics: recall, precision, and ndcg @ k
+
+    Args:
+        model (LighGCN): lightgcn model
+        edge_index (torch.Tensor): 2 by N list of edges for split to evaluate
+        exclude_edge_indices ([type]): 2 by N list of edges for split to discount from evaluation
+        k (int): determines the top k items to compute metrics on
+
+    Returns:
+        tuple: recall @ k, precision @ k, ndcg @ k
+    """
+    user_embedding = model.users_emb.weight
+    item_embedding = model.items_emb.weight
+
+    # get ratings between every user and item - shape is num users x num movies
+    rating = torch.matmul(user_embedding, item_embedding.T)
+
+    for exclude_edge_index in exclude_edge_indices:
+        # gets all the positive items for each user from the edge index
+        user_pos_items = get_user_positive_items(exclude_edge_index)
+        # get coordinates of all edges to exclude
+        exclude_users = []
+        exclude_items = []
+        for user, items in user_pos_items.items():
+            exclude_users.extend([user] * len(items))
+            exclude_items.extend(items)
+
+        # set ratings of excluded edges to large negative value
+        rating[exclude_users, exclude_items] = -(1 << 10)
+
+    # get the top k recommended items for each user
+    _, top_K_items = torch.topk(rating, k=k)
+
+    # get all unique users in evaluated split
+    users = edge_index[0].unique()
+
+    test_user_pos_items = get_user_positive_items(edge_index)
+
+    # convert test user pos items dictionary into a list
+    test_user_pos_items_list = [test_user_pos_items[user.item()] for user in users]
+
+    # determine the correctness of topk predictions
+    r = []
+    for user in users:
+        ground_truth_items = test_user_pos_items[user.item()]
+        label = list(map(lambda x: x in ground_truth_items, top_K_items[user]))
+        r.append(label)
+    r = torch.Tensor(np.array(r).astype("float"))
+
+    recall, precision = RecallPrecision_ATk(test_user_pos_items_list, r, k)
+    ndcg = NDCGatK_r(test_user_pos_items_list, r, k)
+
+    return recall, precision, ndcg
