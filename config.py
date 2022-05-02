64e813958f7ffd075e92894e69717e7386aaa328
Daniel Szemerey <szemy2@gmail.com>
2022-05-02 13:56:58 +0200

    feat: Recall and Precision works now for the Encoder-Decoder model (#88)

--- config.py
+++ config.py
@@ -46,7 +46,7 @@ link_pred_config = Config(
         num_neighbors=64,  # -1 takes all neighbors
         num_neighbors_it=2,
         num_workers=1,
-        candidate_pool_size=10,
+        candidate_pool_size=2,
     ),
     eval_every=1,
     lr_decay_every=1,
