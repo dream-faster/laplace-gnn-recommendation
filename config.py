3066880d7576aebc74fcc3fcbf1f05dddd11c506
Mark Aron Szulyovszky <mark.szulyovszky@gmail.com>
2022-05-02 14:30:26 +0200

    fix(Config): train on the whole graph

--- config.py
+++ config.py
@@ -55,7 +55,7 @@ link_pred_config = Config(
 
 
 lightgcn_config = Config(
-    epochs=1000,
+    epochs=100,
     k=12,
     num_layers=3,  # Number of LightGCN steps
     hidden_layer_size=32,
@@ -98,7 +98,7 @@ only_users_and_articles_nodes = PreprocessingConfig(
     load_text_embedding=False,
     text_embedding_colname="derived_look",
     K=0,
-    data_size=1000,
+    data_size=None,
     save_to_csv=False,
     data_type=DataType.pyg,
 )
