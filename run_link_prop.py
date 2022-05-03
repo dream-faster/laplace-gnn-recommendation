a29eba3ce0fb71cb615843509d22802a8f964cc6
daralthus <peter@kadlot.com>
2022-05-03 05:29:22 +0200

    Rewrite for sparsity

--- run_link_prop.py
+++ run_link_prop.py
@@ -53,35 +53,117 @@ def load_data():
 
 def split_data(start, count, M):
     """Selects a range of users"""
-    return M[start : start + count, :].coalesce("max").to_dense()
+    return M[start : start + count, :].coalesce("max")  # .to_dense()
 
 
-def sample_edges(target, ratio=0.5):
-    """Creates data by dropping random edges."""
-    # mask nodes with degree <= 1 (cloning since you cannot mask twice at the same time)
-    user_deg, item_deg = target.sum(dim=0), target.sum(dim=1)
-    edges = target.clone()
-    edges[:, user_deg <= 1] = 0
-    edges[item_deg <= 1, :] = 0
-    # get edge indices
-    row, col = edges.nonzero(as_tuple=True)
-    assert (target[row, col] == 1).all(), "row, col should be all 1"
+def sparse_assign(m, i, j, val):
+    """Sets a value in a sparse matrix at (i,j)"""
+    assert i < m.size(0) and j < m.size(1), "can only assign to existing indices"
+    row, col, value = m.coo()
+    prev = m[i.item(), j.item()].to_dense().squeeze()
+    return SparseTensor(
+        row=torch.cat((row, torch.tensor([i], dtype=torch.long))),
+        col=torch.cat((col, torch.tensor([j], dtype=torch.long))),
+        value=torch.cat((value, torch.tensor([val], dtype=torch.float) - prev)),
+        sparse_sizes=(m.size(0), m.size(1)),
+    ).coalesce("add")
 
-    # sample some % of edges
-    sample_mask = torch.randint(0, len(row), (len(row),)).unique()[
-        : int(len(row) * ratio)
-    ]
-    # clear max 1 edge for each user and item
-    data = target.clone()
-    # cleared = {}
-    for i, j in torch.stack((row[sample_mask], col[sample_mask])).T:
-        # if i.item() not in cleared and j.item() not in cleared:
-        # assert data[i, j] == 1, "a,b should be 1"
-        # cleared[i.item()] = True
-        # cleared[j.item()] = True
-        data[i, j] = 0.0
 
-    return data, target
+def sparse_fill_on_mask(m, mask, val, dim=0):
+    """Fills masked values in a sparse matrix with a row or col mask. Only sets values that have been explicitly set before."""
+    row, col, value = m.coo()
+    prev_row, prev_col, prev_value = m[mask, :].coo() if dim == 0 else m[:, mask].coo()
+    # restore previous values' indices, assumes mask is boolean and same length as m
+    if dim == 0:
+        prev_row = torch.arange(len(mask))[mask][prev_row]
+    else:
+        prev_col = torch.arange(len(mask))[mask][prev_col]
+    return SparseTensor(
+        row=torch.cat((row, prev_row)),
+        col=torch.cat((col, prev_col)),
+        value=torch.cat((value, val - prev_value)),
+        sparse_sizes=(m.size(0), m.size(1)),
+    ).coalesce("add")
+
+
+def sparse_cat(m, n):
+    """Stack two sparse matrices"""
+    assert m.size(1) == n.size(
+        1
+    ), "cannot stack matrices with different number of columns"
+    row, col, value = n.coo()
+    row = row + m.size(0)
+    m_row, m_col, m_value = m.coo()
+    return SparseTensor(
+        row=torch.cat((m_row, row)),
+        col=torch.cat((m_col, col)),
+        value=torch.cat((m_value, value)),
+        sparse_sizes=(m.size(0) + n.size(0), m.size(1)),
+    ).coalesce("add")
+
+
+def sparse_batch_op(batch_size, op, m, n):
+    """Batch runs any torch or numpy operation on 2 sparse matrices with same dims"""
+    result = SparseTensor(
+        row=torch.tensor([], dtype=torch.long),
+        col=torch.tensor([], dtype=torch.long),
+        value=torch.tensor([], dtype=torch.float),
+        sparse_sizes=(0, m.size(1)),
+    )
+    for i in range(0, m.size(0), batch_size):
+        end = min(i + batch_size, m.size(0))
+        res = SparseTensor.from_dense(
+            op(
+                m[i:end, :].to_dense(),
+                n[i:end, :].to_dense(),
+            )
+        )
+        result = sparse_cat(result, res)
+    return result
+
+
+def sparse_nonzero(m):
+    """Returns the nonzero indices of a sparse matrix"""
+    row, col, value = m.clone().coo()
+    return row[value != 0], col[value != 0], value[value != 0]
+
+
+def intersect2d(a, b):
+    """Returns the intersection of two 2D arrays row by row"""
+    return np.array([np.intersect1d(a[i], b[i]) for i in range(len(a))])
+
+
+def sample_user_items(target, ratio=0.4):
+    """Drop some edges for users that have more than 1 item"""
+    user_deg = target.sum(dim=1)
+    candidate = sparse_fill_on_mask(target, user_deg > 1, 2, dim=0)
+    row, col, value = sparse_nonzero(candidate)
+    rand_mask = (value == 2) & (
+        torch.rand(len(value)) < ((value == 2).sum() / len(value) * ratio)
+    )
+    value[rand_mask] = 0
+    value[value == 2] = 1
+    data = SparseTensor(
+        row=row,
+        col=col,
+        value=value,
+        sparse_sizes=(target.size(0), target.size(1)),
+    ).coalesce("add")
+
+    # take observed out of ground truth
+    # TODO does this create users with 0 items?
+    row, col, value = data.clone().coo()
+    value[value == 1] = 2
+    value[value == 0] = 1
+    value[value == 2] = 0
+    target_new_links = SparseTensor(
+        row=row,
+        col=col,
+        value=value,
+        sparse_sizes=(target.size(0), target.size(1)),
+    ).coalesce("add")
+
+    return data, target_new_links
 
 
 def mean_average_precision(y_true, y_pred, k=12):
@@ -129,16 +211,17 @@ class LinkPropMulti(BaseEstimator):
         )
         self.user_degrees = None
         self.item_degrees = None
-        self.L = None
         self.M = None
-        self.M_target = None
+        self.M_alpha_beta = None
+        self.M_gamma_delta = None
 
-    def fit(self, X, y):
+    def fit(self, X):
         return self
 
-    def fit_for_score(self, M, target):
+    def fit_for_score(self, M):
         # reset model
-        self.L = None
+        self.M_alpha_beta = None
+        self.M_gamma_delta = None
 
         # get node degrees
         self.user_degrees = M.sum(dim=1)
@@ -156,87 +239,77 @@ class LinkPropMulti(BaseEstimator):
         user_gamma[torch.isinf(user_gamma)] = 0.0
         item_delta[torch.isinf(item_delta)] = 0.0
 
-        # outer products
-        alpha_beta = user_alpha.reshape((-1, 1)) * item_beta
-        gamma_delta = user_gamma.reshape((-1, 1)) * item_delta
+        # to keepe sparsity we can calculate the outer product only for the edges
+        # so instead of, outer products: alpha_beta = user_alpha.reshape((-1, 1)) * item_beta
+        # and hadamard product: M_alpha_beta = M * alpha_beta
+        # we can do:
+        row, col, value = sparse_nonzero(M)
+        self.M_alpha_beta = SparseTensor(
+            row=row,
+            col=col,
+            value=user_alpha[row] * item_beta[col] * value,
+            sparse_sizes=(M.size(0), M.size(1)),
+        )
+        self.M_gamma_delta = SparseTensor(
+            row=row,
+            col=col,
+            value=user_gamma[row] * item_delta[col] * value,
+            sparse_sizes=(M.size(0), M.size(1)),
+        )
 
-        # hadamard products
-        M_alpha_beta = M * alpha_beta
-        M_gamma_delta = M * gamma_delta
-        self.L = M_alpha_beta.matmul(M.T).matmul(M_gamma_delta)
+        # (lazy) link propagation, call get_preds_for_users
 
         # get top k new links
-        target_pred = self.predict_k(M, self.k)
+        # TODO iterate over users
+        # target_pred = self.predict_k(M, self.k)
 
         # # with the new links recalculate and store node degrees for next round
         # M_new = (M.clone() + target_pred).clamp(max=1)
         # self.user_degrees = M_new.sum(dim=1)
         # self.item_degrees = M_new.sum(dim=0)
 
-        return M, target, target_pred
+    def get_preds_for_users(self, start, end):
+        return matmul(matmul(self.M_alpha_beta[start:end], M.t()), self.M_gamma_delta)
 
-    def predict_k(self, M, k):
+    def predict_k(self, M, start, end, k):
         """Return top k new links"""
-        # take observered links out of predictions
-        target_pred = (self.L - (M == 1).float() * 100000).clamp(min=0)
-
-        # select top k links for each user
-        for i, col in enumerate(target_pred.topk(k, dim=1)[1]):
-            target_pred[i, col] = 1
-
-        # clear predictions, keep only new links
-        target_pred = (target_pred == 1).float()
-
-        return target_pred
+        user_pred = self.get_preds_for_users(start, end).to_dense()
+        # take observed links out of predictions
+        user_pred = (user_pred - (M[start:end].to_dense() == 1).float() * 100000).clamp(
+            min=0
+        )
+        return user_pred.topk(k, dim=1)[1]
 
     def predict(self, users_ix, k):
         # return self.L[users_ix, :].topk(k, dim=1)[1]
         pass
 
-    def score(self, X, y):
-        # process data
-        M, target, target_pred = self.fit_for_score(X, y)
-        # take observed out of ground truth
-        target_true = (target - (M == 1).float() * 100000).clamp(min=0)
+    # def score(self, X, y):
+    #     return ndcg_score(true_target, target_pred)
 
-        return ndcg_score(target_true, target_pred)
+    def score_mapk(self, X, y, batch_size=400):
+        self.fit_for_score(X)
+        user_topk = torch.empty(size=(0, self.k))
+        target_topk = torch.empty(size=(0, self.k))
+        scores = []
+        for start in tqdm(range(0, X.size(0), batch_size)):
+            end = min(start + batch_size, X.size(0))
+            user_pred = self.predict_k(X, start, end, self.k)
+            target_pred = y[start:end].to_dense().squeeze().topk(self.k, dim=1)[1]
+            user_topk = torch.cat((user_topk, user_pred))
+            target_topk = torch.cat((target_topk, target_pred))
+            scores.append(mean_average_precision(target_topk, user_topk, k=self.k))
+            print(np.array(scores).mean())
 
-    def score_mapk(self, X, y):
-        M, target, target_pred = self.fit_for_score(X, y)
-        # take observed out of ground truth
-        # TODO this creates users with 0 items
-        # TODO also sampling only makes one new item per user
-        target_true = (target - (M == 1).float() * 100000).clamp(min=0)
-        target_t_topk = target_true.topk(self.k, dim=1)[1]
-        # only calculating for users with at least one item in true target
-        target_t = target_t_topk[(target_true.topk(self.k, dim=1)[0] > 0).any(dim=1)]
-        target_p_topk = target_pred.topk(self.k, dim=1)[1]
-        target_p = target_p_topk[(target_true.topk(self.k, dim=1)[0] > 0).any(dim=1)]
-        return (
-            mean_average_precision(target_t_topk, target_p_topk, k=self.k),
-            mean_average_precision(target_t, target_p, k=self.k),
-            target_pred.topk(self.k, dim=1)[1],
-        )
+        # TODO only calculating for users with at least one item in true target
+        # target_t = target_topk[(y[start:end].to_dense().squeeze().topk(self.k, dim=1)[0] > 0).any(dim=1)]
+        return np.array(scores).mean(), user_topk
 
 
 M, src, dest, src_map, dest_map = load_data()
-# split data
-train_size, val_size, test_size = 2000, 1000, 1000
-
-# TODO should we use the original node degrees?
-scores = []
-preds = torch.tensor([], dtype=torch.long)
-total = len(src) // train_size
-for i in tqdm(range(total)):
-    # for i in tqdm(range(2)):
-    batch_size = train_size if i < total - 1 else len(src) - train_size * i
-    data, target = sample_edges(split_data(i * train_size, batch_size, M), 0.8)
-    # data, target = sample_edges(split_data(i * train_size, 2000, M), 0.4)
-    linkProp = LinkPropMulti(rounds=1, k=12, alpha=0, beta=0, gamma=0, delta=0.5)
-    score_with_empty, score, pred = linkProp.score_mapk(data, target)
-    scores.append(score)
-    preds = torch.cat((preds, pred))
-    print(np.array(scores).mean())
+data, target = sample_user_items(M, 0.4)
+linkProp = LinkPropMulti(rounds=1, k=12, alpha=0.1, beta=0.1, gamma=0.2, delta=0.5)
+score, preds = linkProp.score_mapk(data, target)
 submission = pd.DataFrame(preds.apply_(lambda x: dest[x])).astype("string")
 submission["prediction"] = (
     submission[[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]]
@@ -245,8 +318,10 @@ submission["prediction"] = (
 )
 submission["customer_id"] = src[: len(submission)]
 submission[["customer_id", "prediction"]].to_csv(
-    "data/derived/submission_2.csv", index=False
+    "data/derived/submission_3.csv", index=False
 )
+
+# TODO find optimal params
 # param_grid = [
 #     {
 #         "alpha": [0.0, 0.5],
