9206cf088486dacf40a32bed3f603380f5ed3748
Mark Aron Szulyovszky <mark.szulyovszky@gmail.com>
2022-05-04 11:44:19 +0200

    fix(Matcher): variable name

--- data/matching/popular_items.py
+++ data/matching/popular_items.py
@@ -2,12 +2,13 @@ from numpy import dtype
 from .type import Matcher
 import torch
 
+
 class PopularItemsMatcher(Matcher):
     def __init__(self, k: int):
-        self.location_for_user = torch.Tensor(
+        self.popular_items = torch.Tensor(
             torch.load("data/derived/most_popular_products.pt")
         ).to(torch.long)
         self.k = k
 
     def get_matches(self, user_id: int) -> torch.Tensor:
-        return self.location_for_user[: self.k]
+        return self.popular_items[: self.k]
