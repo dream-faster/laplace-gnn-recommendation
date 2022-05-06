fix(Training): Added embedding layer normalization (#116)
* feat: Programmatically adding arguments to parser from config.

* feat: Seperated out hidden layer size and encoder output, changed the values of embedding categories, added relu on the last layer of the encoder.

* feat: Further decreasing embedding size.

* fix: Exchanged to sigmoid.

* feat: Took out relu and/or sigmoid from encoder last layer.

* Update config.py

* Update config.py

* Update config.py

* Update config.py

Co-authored-by: Mark Aron Szulyovszky <mark.szulyovszky@gmail.com>

model/encoder_decoder.py
@@ -74,16 +74,18 @@ class Encoder_Decoder_Model(torch.nn.Module):
 
             embedding_customers = [
                 Embedding(
-                    int(customer_info.num_cat[i] + 1),
-                    int(customer_info.embedding_size[i]),
+                    num_embeddings = int(customer_info.num_cat[i] + 1),
+                    embedding_dim = int(customer_info.embedding_size[i]),
+                    max_norm = 1
                 )
                 for i in range(customer_info.num_feat)
             ]
 
             embedding_articles = [
                 Embedding(
-                    int(article_info.num_cat[i] + 1),
-                    int(article_info.embedding_size[i]),
+                    num_embeddings = int(article_info.num_cat[i] + 1),
+                    embedding_dim = int(article_info.embedding_size[i]),
+                    max_norm = 1
                 )
                 for i in range(article_info.num_feat)
             ]
@@ -122,7 +124,8 @@ class Encoder_Decoder_Model(torch.nn.Module):
         if self.embedding:
             x_dict = self.__embedding(x_dict)
         z_dict = self.encoder(x_dict, edge_index_dict)
-        return self.decoder(z_dict, edge_label_index)
+        output = self.decoder(z_dict, edge_label_index)
+        return output
 
 
     def infer(self,  x_dict, edge_index_dict: dict, edge_label_index: torch.Tensor)->Tensor:

run_pipeline.py
@@ -39,11 +39,11 @@ def run_pipeline(config: Config):
         encoder_layers=get_SAGEConv_layers(
             num_layers=config.num_layers,
             hidden_channels=config.hidden_layer_size,
-            out_channels=config.hidden_layer_size,
+            out_channels=config.encoder_layer_output_size,
         ),
         decoder_layers=get_linear_layers(
             num_layers=config.num_layers,
-            in_channels=config.hidden_layer_size * 2,
+            in_channels=config.encoder_layer_output_size * 2,
             hidden_channels=config.hidden_layer_size,
             out_channels=1,
         ),
@@ -81,6 +81,9 @@ def run_pipeline(config: Config):
         else:
             print("| Saving Best Generalized Model...")
             torch.save(model, f"model/saved/model_final.pt")
+            old_val_precision = (
+                -1
+            )  # We should only save it at the inflection point from decreasing one step
 
         if epoch % int(config.epochs * config.save_every) == 0:
             print("| Saving Model at a regular interval...")