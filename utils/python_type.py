cad78c6e97ec7b7a1ae71a4c202edb59d4452414
Daniel Szemerey <daniel.szemerey.15@ucl.ac.uk>
2022-05-07 15:39:59 +0200

    feat: Added another method.

--- utils/python_type.py
+++ utils/python_type.py
@@ -1,14 +1,20 @@
+import os
+
+
 def isnotebook():
-    try:
-        shell = get_ipython().__class__.__name__
-        print(shell)
-        if shell == "ZMQInteractiveShell":
-            return True  # Jupyter notebook or qtconsole
-        elif shell == "google.colab._shell":
-            return True  # Google Colab Shell
-        elif shell == "TerminalInteractiveShell":
-            return False  # Terminal running IPython
-        else:
-            return False  # Other type (?)
-    except NameError:
-        return False  # Probably standard Python interpreter
+    if "COLAB_GPU" in os.environ:
+        print("I'm running on Colab")
+    else:
+        try:
+            shell = get_ipython().__class__.__name__
+            print(shell)
+            if shell == "ZMQInteractiveShell":
+                return True  # Jupyter notebook or qtconsole
+            elif shell == "google.colab._shell":
+                return True  # Google Colab Shell
+            elif shell == "TerminalInteractiveShell":
+                return False  # Terminal running IPython
+            else:
+                return False  # Other type (?)
+        except NameError:
+            return False  # Probably standard Python interpreter
