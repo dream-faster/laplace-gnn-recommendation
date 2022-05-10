5faa873b4e1bd0099abc31ee91a9fd996eb8987d
Mark Aron Szulyovszky <mark.szulyovszky@gmail.com>
2022-05-10 23:04:35 +0200

    fix(Tests): passing in the right config to GraphDataset

--- tests/actual_data.py
+++ tests/actual_data.py
@@ -9,7 +9,7 @@ from tests.test_utils import get_raw_sample
 def create_data():
     data_dir = "data/derived/"
     train_dataset = GraphDataset(
-        config=link_pred_config.dataloader_config,
+        config=link_pred_config,
         edge_path=data_dir + "dummy_edges_train.pt",
         graph_path=data_dir + "dummy_graph_train.pt",
         article_edge_path=data_dir + "dummy_rev_edges_train.pt",
