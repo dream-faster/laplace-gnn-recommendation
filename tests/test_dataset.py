afcd2a9e064e7ba4cf951983554a3640da5ba346
Daniel Szemerey <daniel.szemerey.15@ucl.ac.uk>
2022-05-10 17:37:20 +0200

    feat: Dataset needed to be fixed, but this is not the most up-to-date.

--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -8,26 +8,32 @@ from tests.dummy_data import create_dummy_data, get_raw_data
 from tests.actual_data import create_data
 from torch_geometric import seed_everything
 
+seed_everything(5)
+data = create_data()
+
 
 def edge_features(data: HeteroData):
 
-    # Basic Testing of edges
+    # Basic Testing of edges if they fit size expectations
     edges = data[Constants.edge_key]
+    assert edges.edge_index.shape[0] == 2
+    assert edges.edge_index.shape[1] > 0
     assert edges.edge_label_index.shape[0] == 2
+    assert edges.edge_label_index.shape[1] > 0
     assert edges.edge_label.shape[0] == 2
-    assert len(edges.label.shape[0]) == 1
-
-    assert edges.edge_label_index.shape[1] == 2
+    assert edges.edge_label.shape[0] > 0
+    assert len(edges.edge_label.shape) == 1
+    assert edges.edge_label.shape[0] == edges.edge_label_index.shape[1]
 
     # Assertions for reverse edges
 
 
-def node_features(data: HeteroData, raw_data: dict):
+def node_features(data: HeteroData):
     user_features, article_features = (
         data[Constants.node_user].x,
         data[Constants.node_item].x,
     )
-    print(data)
+    # print(data)
     print(data[Constants.edge_key].edge_index)
     print(data[Constants.edge_key].edge_label_index)
     print(data[Constants.edge_key].edge_label)
@@ -40,31 +46,21 @@ def node_features(data: HeteroData, raw_data: dict):
     assert torch.equal(
         article_features.type(torch.float),
         torch.tensor(
-            [[0.1, 0.2, 0.3, 0.4], [1.1, 2.2, 2.3, 2.4], [2.1, 2.2, 2.3, 2.4]]
+            [
+                [0.1, 0.2, 0.3, 0.4],
+                [1.1, 1.2, 1.3, 1.4],
+                [2.1, 2.2, 2.3, 2.4],
+            ]
         ).type(torch.float),
     )
 
 
 def test_integrity_nodes():
-    seed_everything(5)
-    data = create_data()
-    _, _, raw_all = create_dummy_data()
-    node_features(data, raw_all)
-    # edge_features(data, raw_data)
+    node_features(data)
 
 
 def test_integrity_edges():
-    seed_everything(5)
-    data = create_data()
-    _, _, raw_all = create_dummy_data()
-    assert True
-    # edge_features(data, raw_data)
-
-
-# def test_integrity_dummy():
-#     data, raw_data = create_dummy_data()
-#     node_features(data, raw_data)
-#     # edge_features(data, raw_data)
+    edge_features(data)
 
 
 if __name__ == "__main__":
