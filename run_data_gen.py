c3b4fefc2117e6b7ba7b382057a0b93aa73eadbc
Daniel Szemerey <daniel.szemerey.15@ucl.ac.uk>
2022-05-11 18:39:50 +0200

    feat: Dataset creation now automatically extracts ids and features.

---
+++ run_data_gen.py
@@ -0,0 +1,4 @@
+from tests.data_generator import create_dummy_data
+
+
+create_dummy_data()
--- tests/data_generator.py
+++ tests/data_generator.py
@@ -1,9 +1,15 @@
+from numpy import column_stack
 from torch_geometric.data import HeteroData
 from utils.constants import Constants
 import torch
 from torch import Tensor
 from .util import get_raw_sample, get_raw_all
 from utils.types import NodeFeatures, ArticleFeatures, AllEdges, SampledEdges, Labels
+import pandas as pd
+
+
+def extract_edges(transactions: pd.DataFrame, by: str, get: str) -> dict:
+    return transactions.groupby(by)[get].apply(list).to_dict()
 
 
 def create_dummy_data(save=False):
@@ -22,10 +28,23 @@ def create_dummy_data(save=False):
 
     data[Constants.edge_key].edge_index = graph_edges
 
-    edges_dict = {0: [0, 2], 1: [1]}
-    rev_edges_dict = {0: [0], 1: [1], 2: [0]}
+    graph_edges_pd = (
+        pd.DataFrame(graph_edges.numpy())
+        .transpose()
+        .rename({0: "user", 1: "article"}, axis=1)
+    )
+    edges_dict = extract_edges(
+        graph_edges_pd,
+        by="user",
+        get="article",
+    )
+    rev_edges_dict = extract_edges(
+        graph_edges_pd,
+        by="article",
+        get="user",
+    )
 
-    subgraph_data = create_final_subgraph(data, edges_dict, rev_edges_dict, n_hop=2)
+    subgraph_data = __create_final_subgraph(data, edges_dict, rev_edges_dict, n_hop=2)
     raw_data = get_raw_sample(data)
     raw_all = get_raw_all(data)
 
@@ -41,24 +60,55 @@ def save_dummy_data():
     create_dummy_data(save=True)
 
 
-def create_final_subgraph(
+def __deconstruct_heterodata(
+    hdata: HeteroData,
+) -> tuple[Tensor, Tensor, Tensor]:
+    return (
+        hdata[Constants.node_user].x,
+        hdata[Constants.node_item].x,
+        hdata[Constants.edge_key].edge_index,
+    )
+
+
+def __create_final_subgraph(
     entire_graph: HeteroData, edges_dict: dict, rev_edges_dict: dict, n_hop: int
 ) -> HeteroData:
-    user_nodes = [0]
-    user_nodes_list, article_nodes_list = [], []
+    user_id = Tensor([0])
+    art_id = torch.empty(0)
 
+    """ Collect ids """
     for _ in range(n_hop):
-        article_nodes = [edges_dict[i] for i in user_nodes]
-        user_nodes = [rev_edges_dict[i] for i in article_nodes]
-        user_nodes_list.append(user_nodes)
-        article_nodes_list.append(article_nodes)
+        art_id = torch.unique(
+            torch.cat(
+                [
+                    art_id,
+                    Tensor([i for id in user_id for i in edges_dict[id.item()]]),
+                ]
+            )
+        )
+        user_id = torch.unique(
+            torch.cat(
+                [
+                    user_id,
+                    Tensor([i for id in art_id for i in rev_edges_dict[id.item()]]),
+                ]
+            )
+        )
 
-    article_ids = torch.unique(torch.tensor(article_nodes_list))
-    user_ids = torch.unique(torch.tensor(user_nodes_list))
+    user_features, article_features, edge_index = __deconstruct_heterodata(entire_graph)
+
+    """ Get Features """
+    user_features, article_features = (
+        user_features[user_id],
+        article_features[art_id],
+    )
+
+    """ Get Edges """
+    graph_edges = [edge_index[:, edge_index[0] == user_id] for user_id in user_id]
 
     """Create Data"""
     subgraph = HeteroData()
-    subgraph[Constants.node_user].x = node_features
+    subgraph[Constants.node_user].x = user_features
     subgraph[Constants.node_item].x = article_features
 
     # Add original directional edges
@@ -90,7 +140,7 @@ def __get_raw_data() -> tuple[
             torch.tensor([4.0, 4.1, 4.2, 4.3, 4.4]),
         ]
     )
-    graph_edges = torch.tensor([[0, 0, 0, 1, 2], [0, 2, 4, 1, 3]])
+    graph_edges = torch.tensor([[0, 0, 0, 1, 2, 2], [0, 2, 4, 1, 3, 0]])
 
     # Last sample: We simulate how the last edge is taken from the larger graph, 5 > len(article_features)
     sampled_edges = torch.tensor([[0, 0, 0], [2, 3, 5]])
