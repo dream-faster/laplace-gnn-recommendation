f4e52fbee7ac920094e0f4b13d06e6f568afc28c
Daniel Szemerey <daniel.szemerey.15@ucl.ac.uk>
2022-05-12 11:06:42 +0200

    chore: VSCode handles this, no need for it.

--- run_test_debug.py
+++
@@ -1,7 +0,0 @@
-from tests.data_generator import create_entire_graph_data
-from tests.test_dataset import test_integrity_edges, test_integrity_nodes
-
-
-# create_entire_graph_data()
-test_integrity_edges()
-test_integrity_nodes()
