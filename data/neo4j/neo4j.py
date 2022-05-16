fd93e356743777fb5e83d156cb91ebff0ece88b9
Mark Aron Szulyovszky <mark.szulyovszky@gmail.com>
2022-05-16 22:19:07 +0200

    chore(Neo4J): cleanup old import mechanism

--- data/neo4j/neo4j.py
+++ data/neo4j/neo4j.py
@@ -9,7 +9,6 @@ class App:
         self.driver = GraphDatabase.driver(uri, auth=(user, password))
 
     def close(self):
-        # Don't forget to close the driver connection when you are finished with it
         self.driver.close()
 
     """ GET """
@@ -34,35 +33,6 @@ class App:
         with self.driver.session() as session:
             info = session.run(query)
             print(info)
-
-    def create_constraints(self):
-        self.run_query(
-            "CREATE CONSTRAINT unique_user_id IF NOT EXISTS FOR (user:User) REQUIRE user._id IS UNIQUE"
-        )
-        self.run_query(
-            "CREATE CONSTRAINT unique_article_id IF NOT EXISTS FOR (article:Article) REQUIRE article._id IS UNIQUE"
-        )
-
-    def load_articles_csv(self):
-        query = (
-            query_periodic_commit
-            + "LOAD CSV FROM 'https://storage.googleapis.com/heii-public/neo4j/articles.csv' AS row WITH row[0] AS id MERGE (a:Article {_id: id}) RETURN count(a);"
-        )
-        self.run_query(query)
-
-    def load_customers_csv(self):
-        query = (
-            query_periodic_commit
-            + "LOAD CSV FROM 'https://storage.googleapis.com/heii-public/neo4j/customers.csv' AS row WITH row[0] AS id MERGE (c:Customer {_id: id}) RETURN count(c);"
-        )
-        self.run_query(query)
-
-    def load_relationships(self):
-        query = (
-            query_periodic_commit
-            + "LOAD CSV WITH HEADERS FROM 'https://storage.googleapis.com/heii-public/neo4j/transactions.csv' AS row WITH row.customer_id AS customer_id, row.article_id AS article_id MATCH (c:Customer {_id: customer_id}) MATCH (a:Article {_id: article_id}) MERGE (c)-[rel:BUYS]->(a) RETURN count(rel);"
-        )
-        self.run_query(query)
 
     def clear(self):
         query = "MATCH (n) DETACH DELETE n"
