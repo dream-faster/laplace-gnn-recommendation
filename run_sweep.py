b384e3bf61e400065cc596e834b0a1348bbd9782
Mark Aron Szulyovszky <mark.szulyovszky@gmail.com>
2022-05-19 16:52:17 +0200

    fix(HPO): eval_every == no of epochs

--- run_sweep.py
+++ run_sweep.py
@@ -4,7 +4,7 @@ from config import link_pred_config
 config = link_pred_config
 config.epochs = 5
 config.k = 40
-config.eval_every = 4
+config.eval_every = config.epochs
 config.evaluate_break_at = 50
 config.batch_size = 128
 config.wandb_enabled = True
