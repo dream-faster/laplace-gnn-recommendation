083b255a5479ae886838c359d7a81d824abf5f95
Mark Aron Szulyovszky <mark.szulyovszky@gmail.com>
2022-05-20 13:33:54 +0200

    fix(Config): k = 30

--- run_sweep.py
+++ run_sweep.py
@@ -3,7 +3,7 @@ from config import link_pred_config
 
 config = link_pred_config
 config.epochs = 5
-config.k = 40
+config.k = 30
 config.eval_every = config.epochs
 config.evaluate_break_at = 50
 config.batch_size = 128
